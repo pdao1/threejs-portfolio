6108a4424000a52a4766b52b48a6908712dcf3bb
pdao1 <69880265+pdao1@users.noreply.github.com>
2022-12-13 21:11:07 -1000

    Initial commit

---
+++ main.js
@@ -0,0 +1,77 @@
+import './style.css'
+
+import * as THREE from 'three';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer({
+	canvas: document.querySelector('#bg'),
+})
+
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+camera.position.setZ(30);
+
+renderer.render( scene, camera )
+
+const geometry = new THREE.SphereGeometry( 10, 60, 60 )
+const material = new THREE.MeshStandardMaterial( { color: 0xFF0000 } );
+const torus = new THREE.Mesh( geometry, material )
+
+scene.add(torus)
+
+// lighting
+// const pointLight = new THREE.PointLight(0xFFFFFF)
+// pointLight.position.set( 30,100,100 )
+
+// const ambientLight = new THREE.AmbientLight(0xFFFFFF);
+
+const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1);
+directionalLight.position.set( 30, 50, 50);
+scene.add( directionalLight );
+
+// // helpers
+// const lightHelper = new THREE.PointLightHelper(pointLight)
+// scene.add(lightHelper)
+
+// const gridHelper = new THREE.GridHelper(200, 50);
+// scene.add(gridHelper)
+
+const directionalHelper = new THREE.DirectionalLightHelper( directionalLight, 10 );
+scene.add( directionalHelper );
+
+// orbit control
+const controls = new OrbitControls(camera, renderer.domElement); 
+
+function addStars() {
+	const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+	const material = new THREE.MeshStandardMaterial( {color: 0xFFFFFF} )
+	const star = new THREE.Mesh( geometry, material );
+
+	const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(600) );
+
+	star.position.set(x, y, z)
+	scene.add(star)
+}
+
+Array(1200).fill().forEach(addStars)
+
+// textures
+// const bgTexture = new THREE.TextureLoader().load('space.jpg');
+// scene.background(bgTexture)
+
+// recursion to repeat animation and register changes between frames
+function animate() {
+	requestAnimationFrame( animate );
+
+	torus.rotation.x += 0.01;
+	torus.rotation.y += 0.005;
+	torus.rotation.z += 0.01;
+	
+	controls.update()
+	renderer.render( scene, camera );
+}
+animate();
